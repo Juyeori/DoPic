e15ca312c3866f156bbb83d231a464780505cc47
Juyeori <dlwndus0728@ajou.ac.kr>
2023-05-15 18:57:38 +0900

    사진 등록 임시 작업 중

--- ai/AIHairDiagnosisScreen.js
+++ ai/AIHairDiagnosisScreen.js
@@ -1,11 +1,46 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 const AIHairDiagnosisScreen = () => {
+  const handleTakePhoto = async () => {
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+    if (status === 'granted') {
+      const result = await ImagePicker.launchCameraAsync();
+      if (!result.cancelled) {
+        // Do something with the captured photo
+        console.log(result);
+      }
+    } else {
+      // Camera permission denied
+      console.log('Camera permission denied');
+    }
+  };
+
+  const handleUploadPhoto = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status === 'granted') {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) {
+        // Do something with the selected photo
+        console.log(result);
+      }
+    } else {
+      // Media library permission denied
+      console.log('Media library permission denied');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.box}>
         <Text style={styles.text}>AIHairDiagnosisScreen</Text>
+        <TouchableOpacity style={styles.button} onPress={handleTakePhoto}>
+          <Text style={styles.buttonText}>Take Photo</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleUploadPhoto}>
+          <Text style={styles.buttonText}>Upload Photo</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -24,6 +59,20 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#2196F3',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 
